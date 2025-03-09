69b385b078efb5fe8f4d1093b2c6f659a467045f
Mohammed3927 <gkhal839@gmail.com>
2025-03-09 21:23:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const client = new Client({
     }),
 });
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 client.once('ready', () => {
 
     console.log('bot is ready ${client.user.tag}');
