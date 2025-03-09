54c765e3e60365b6f363b534c730de67ce02ddb8
Mohammed3927 <gkhal839@gmail.com>
2025-03-09 21:15:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 const { Client, GatewayIntentBits, Collection, REST, Routes } = require('discord.js');
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
-const axios = require('axios');
-const { DisTube } = require('distube');
-const { YtDlpPlugin } = require('@distube/yt-dlp');
-const { SpotifyPlugin } = require('@distube/spotify');
-const { SoundCloudPlugin } = require('@distube/soundcloud');
-const { Dynamic } = require('musicard');
-const config = require('./config.json');
-const musicIcons = require('./UI/icons/musicicons');
-const colors = require('./UI/colors/colors');
-const loadLogHandlers = require('./logHandlers');
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 const client = new Client({
     intents: Object.keys(GatewayIntentBits).map((a) => {
